937faceca47fb8d56612df3beb5e78e99d14e8cc
Ed Esquivel <edgaresquivel0615@gmail.com>
2024-05-15 19:18:31 -0500

    Hello from Express

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = process.env.PORT || 3000; //puerto de red cualquiera despuès de 1024
+const app = express(); // se crea instancia
+ 
+app.listen(port, function () { // escuchar peticiones http en "port" 
+    console.log("Server is listening at port: " + port); //mensaje si conexion es correcta
+});
+
+app.get('/', function (req, res) { // si hay peticion http "get" en la raíz "/"
+    res.send("hello world"); // respuesta a peticion
+});
